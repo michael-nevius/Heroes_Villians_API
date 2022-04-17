ae63f1556fd0c9964041baa4e849c2170c56bf01
michael-nevius <michael.a.nevius@gmail.com>
2022-04-17 11:12:56 -0400

    Created my supers views function + serializers file + url file

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import request
+from .models import Super
+from .serializers import SuperSerializer
+from rest_framework import status
+from django.http import Http404
 
-# Create your views here.
+class SupersList(APIView):
+
+    def get(self, request):
+        type_param = request.query_params.get('type')
+        supers = Super.objects.all()
+
+        if type_param:
+            supers = supers.filter(super_type__type=type_param)
+        else:
+            supers = supers.order_by('super_type')
+
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = SuperSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SupersDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Super.objects.get(pk=pk)
+        except Super.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        super = self.get_object(pk=pk)
+        serializer = SuperSerializer(super)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        super = self.get_object(pk)
+        serializer = SuperSerializer(super, data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        super = self.get_object(pk)
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
